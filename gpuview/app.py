ef1720ce3c80e2ecd029572d28db2b35f3edcd62
iotZHY <iotzhy@163.com>
2023-05-31 16:24:55 +0800

    bottle --> flask, mod app.py

--- gpuview/app.py
+++ gpuview/app.py
@@ -11,25 +11,60 @@ import os
 import json
 from datetime import datetime
 
-from bottle import Bottle, TEMPLATE_PATH, template, response
+from flask import redirect, url_for, render_template, request, Flask, jsonify
+from flask_login import LoginManager, login_required, logout_user, login_user, current_user
 
 from . import utils
 from . import core
+from . import login
 
-
-app = Bottle()
 abs_path = os.path.dirname(os.path.realpath(__file__))
 abs_views_path = os.path.join(abs_path, 'views')
-TEMPLATE_PATH.insert(0, abs_views_path)
+app = Flask(__name__, template_folder=abs_views_path)
 
 EXCLUDE_SELF = False  # Do not report to `/gpustat` calls.
 
 
+app.secret_key = 'abc'
+login_manager = LoginManager()  # 实例化登录管理对象
+login_manager.init_app(app)  # 初始化应用
+login_manager.login_view = 'login'  # 设置用户登录视图函数 endpoint
+
+
+@app.route('/login/', methods=('GET', 'POST'))  # 登录
+def login():
+    if request.method == 'POST':
+        user_name = request.form.get('username')
+        password = request.form.get('password')
+        user_info = login.get_user(user_name)  # 从用户数据中查找用户记录
+        if user_info is None:
+            emsg = "用户名或密码密码有误"
+        else:
+            user = login.User(user_info)  # 创建用户实体
+            if user.verify_password(password):  # 校验密码
+                login_user(user)  # 创建用户 Session
+                return redirect(request.args.get('next') or url_for('index'))
+            else:
+                emsg = "用户名或密码密码有误"
+    return render_template('login.html')
+
+
+@login_manager.user_loader  # 定义获取登录用户的方法
+def load_user(user_id):
+    return login.User.get(user_id)
+
+@app.route('/logout')  # 登出
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/')
+@login_required
 def index():
     gpustats = core.all_gpustats()
     now = datetime.now().strftime('Updated at %Y-%m-%d %H-%M-%S')
-    return template('index', gpustats=gpustats, update_time=now)
+    return render_template('index.html', gpustats=gpustats, update_time=now, username = current_user.username)
 
 
 @app.route('/gpustat', methods=['GET'])
@@ -45,12 +80,11 @@ def report_gpustat():
         else:
             raise TypeError(type(obj))
 
-    response.content_type = 'application/json'
     if EXCLUDE_SELF:
         resp = {'error': 'Excluded self!'}
     else:
         resp = core.my_gpustat()
-    return json.dumps(resp, default=_date_handler)
+    return jsonify(resp)
 
 
 def main():
